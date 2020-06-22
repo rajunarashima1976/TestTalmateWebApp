6c9e67b540518050efa98245c44be3d6b73dc2a8
User6 <User6@CBAPOOL-0>
2020-06-22 09:27:13 +0000

    Added a new Class in Model for Testing

---
+++ CBA.Training.Talmate.Web/Models/Class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CBA.Training.Talmate.Web.Models
+{
+    public class Class
+    {
+        public int neweid { get; set; }
+    }
+}
